64b02bba8342d65c5a5ace2e0cd0cf42d71463db
Dee <deeit789@gmail.com>
2023-10-08 19:49:59 +0700

    .

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 require("dotenv").config();
-const cors = require("cors");
 
 const port = process.env.PORT || 8888;
 
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
-
-app.use(cors());
+const io = socketIo(server, { cors: {} });
 
 require("./src/sockets/socketHandlers")(io);
 
